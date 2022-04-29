79dd16fb2b6adad1f881852cf262a1bbbd530b5e
laianribas <laian67a@gmail.com>
2022-04-29 19:56:53 -0300

    teste commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,5 +9,5 @@ app.use(express.json())
 // ... your REST API routes will go here
 
 app.listen(3000, () =>
-  console.log('REST API server ready at: http://localhost:3000'),
+  console.log('REST API rodando: http://localhost:3000'),
 )
