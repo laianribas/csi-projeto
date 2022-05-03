a9819ee0bcb8378804f717c44e3ea29455c04f2b
laianribas <laian67a@gmail.com>
2022-05-02 22:22:24 -0300

    hot reload

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,54 +6,9 @@ const app = express()
 
 app.use(express.json())
 
-async function main() {
-  await prisma.setor.create({
-    data: {
-      nome: 'prisma2',
-      ramal: 'teste2',
-      descricao: 'teste2',
-    },
-  })
-
-  const todosSetores = await prisma.setor.findMany()
-  todosSetores.map(setor => {
-    console.log(setor.id)
-  })
-  const funcionarioCriado = await prisma.funcionario.create({
-    data: {
-      nome: 'Rodrigo',
-      nivel_acesso: 'funcionario',
-      campus: 'Jequié',
-      login: 'bau123',
-      senha: 'bau123',
-      primeiro_acesso: true,
-      setores: {
-        create: todosSetores.map(setorAux => ({
-          setor: {
-            connect: {
-              id: setorAux.id
-            }
-          }
-        }))
-      }
-    },
-  })
-
-  const funcionario = await prisma.funcionario.findMany({ include: { setores: true } })
-  console.log(await prisma.setor.count())
-  console.dir(todosSetores, { depth: null })
-  console.log(await prisma.funcionario.count())
-  console.dir(funcionario, { depth: null })
-}
-
-main()
-  .catch((e) => {
-    throw e
-  })
-  .finally(async () => {
-    await prisma.$disconnect()
-  })
-
+app.get('/', (req, res) => {
+  res.send('Hello world@!!')
+})
 app.listen(3000, () =>
   console.log('REST API rodando: http://localhost:3000'),
 )
