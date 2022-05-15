e0556cffd7805862511cd912221bb0f6935cf34e
laianribas <laian67a@gmail.com>
2022-05-15 11:51:40 -0300

    rotas já implementadas

--- backend/routes/Rotas_Chamado.ts
+++ backend/routes/Rotas_Chamado.ts
@@ -1,14 +1,24 @@
 import { Router } from "express";
 import { ControllerCadastrarChamado } from "../controllers/Chamado/Controller_Cadastrar_Chamado";
+import { ControllerEditarChamado } from "../controllers/Chamado/Controller_Editar_Chamado";
+import { ControllerEditarStatusChamado } from "../controllers/Chamado/Controller_Editar_Status_Chamado";
+import { ControllerResgatarChamado } from "../controllers/Chamado/Controller_Resgatar_Chamado";
 import { ControllerResgatarTodosChamados } from "../controllers/Chamado/Controller_Resgatar_Todos_Chamados";
 import { verificarToken } from "../middlewares/verificarToken";
 
-
+//Fazer rota de chamados pelo funcionário logado
 const RotasChamado = Router()
 const controllerCadastrarChamado = new ControllerCadastrarChamado()
+const controllerEditarChamado = new ControllerEditarChamado()
+const controllerEditarStatusChamado = new ControllerEditarStatusChamado()
 const controllerResgatarTodosChamados = new ControllerResgatarTodosChamados()
+const controllerResgatarChamado = new ControllerResgatarChamado()
 
-RotasChamado.get('/', verificarToken, controllerResgatarTodosChamados.handle)
 RotasChamado.post('/', verificarToken, controllerCadastrarChamado.handle)
+RotasChamado.patch('/:id', verificarToken, controllerEditarChamado.handle)
+RotasChamado.patch('/status/:id', verificarToken, controllerEditarStatusChamado.handle)
+RotasChamado.get('/:id', verificarToken, controllerResgatarChamado.handle)
+RotasChamado.get('/', verificarToken, controllerResgatarTodosChamados.handle)
 
 export { RotasChamado };
+
