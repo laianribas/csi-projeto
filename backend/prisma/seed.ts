d1351a5d7cbbf24324c54bdd54e65036fad57843
laianribas <laian67a@gmail.com>
2022-05-24 19:27:10 -0300

    seed

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from "@prisma/client"
+const prisma = new PrismaClient()
+
+async function main() {
+  const setor = await prisma.setor.create({
+    data: {
+      nome: 'DCE',
+      ativo: true,
+      descricao: 'Departamento de Ciências Exatas',
+      ramal: '156165789'
+    }
+  })
+  const status = await prisma.status.create({
+    data: {
+      descricao: 'Em aberto',
+      updatedAt: new Date(),
+    }
+  })
+  console.log(setor, status)
+}
+
+main()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
