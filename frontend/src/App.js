43c811b16543eff19280431ab76cc7d4b5f8a615
laianribas <laian67a@gmail.com>
2022-06-11 22:19:42 -0300

    hot reload dockerizado funcionando

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import Alert from 'react-bootstrap/Alert'
+import { Button } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
-      <Alert variant="danger">cuaaabbbbb</Alert>
+      <Alert variant="warning">dmsadmsaio</Alert>
+      <Button>aro aro aro aaaaaaaaaaaa</Button>
     </div>
   )
 }
