df6cc5a54e4f71ea4cffd731e921f411d0b982cf
laianribas <laian67a@gmail.com>
2022-06-26 22:00:52 -0300

    header

--- frontend/src/components/Navbars/AdminNavbarLinks.jsx
+++ frontend/src/components/Navbars/AdminNavbarLinks.jsx
@@ -1,5 +1,5 @@
 // Chakra Icons
-import { BellIcon, SearchIcon } from "@chakra-ui/icons";
+import { BellIcon, SearchIcon } from '@chakra-ui/icons'
 // Chakra Imports
 import {
   Button,
@@ -13,96 +13,54 @@ import {
   MenuItem,
   MenuList,
   Text,
-  useColorModeValue,
-} from "@chakra-ui/react";
+  useColorModeValue
+} from '@chakra-ui/react'
 // Assets
-import avatar1 from "assets/img/avatars/avatar1.png";
-import avatar2 from "assets/img/avatars/avatar2.png";
-import avatar3 from "assets/img/avatars/avatar3.png";
+import avatar1 from 'assets/img/avatars/avatar1.png'
+import avatar2 from 'assets/img/avatars/avatar2.png'
+import avatar3 from 'assets/img/avatars/avatar3.png'
 // Custom Icons
-import { ProfileIcon, SettingsIcon } from "components/Icons/Icons";
+import { ProfileIcon, SettingsIcon } from 'components/Icons/Icons'
 // Custom Components
-import { ItemContent } from "components/Menu/ItemContent";
-import SidebarResponsive from "components/Sidebar/SidebarResponsive";
-import PropTypes from "prop-types";
-import React from "react";
-import { NavLink } from "react-router-dom";
-import routes from "routes.js";
+import { ItemContent } from 'components/Menu/ItemContent'
+import SidebarResponsive from 'components/Sidebar/SidebarResponsive'
+import PropTypes from 'prop-types'
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import routes from 'routes.js'
 
 export default function HeaderLinks(props) {
-  const { variant, children, fixed, secondary, onOpen, ...rest } = props;
+  const { variant, children, fixed, secondary, onOpen, ...rest } = props
 
   // Chakra Color Mode
-  let mainTeal = useColorModeValue("teal.300", "teal.300");
-  let inputBg = useColorModeValue("white", "gray.800");
-  let mainText = useColorModeValue("gray.700", "gray.200");
-  let navbarIcon = useColorModeValue("gray.500", "gray.200");
-  let searchIcon = useColorModeValue("gray.700", "gray.200");
+  let mainTeal = useColorModeValue('teal.300', 'teal.300')
+  let inputBg = useColorModeValue('white', 'gray.800')
+  let mainText = useColorModeValue('gray.700', 'gray.200')
+  let navbarIcon = useColorModeValue('gray.500', 'gray.200')
+  let searchIcon = useColorModeValue('gray.700', 'gray.200')
 
   if (secondary) {
-    navbarIcon = "white";
-    mainText = "white";
+    navbarIcon = 'white'
+    mainText = 'white'
   }
-  const settingsRef = React.useRef();
+  const settingsRef = React.useRef()
   return (
     <Flex
-      pe={{ sm: "0px", md: "16px" }}
-      w={{ sm: "100%", md: "auto" }}
+      pe={{ sm: '0px', md: '16px' }}
+      w={{ sm: '100%', md: 'auto' }}
       alignItems="center"
       flexDirection="row"
     >
-      <InputGroup
-        cursor="pointer"
-        bg={inputBg}
-        borderRadius="15px"
-        w={{
-          sm: "128px",
-          md: "200px",
-        }}
-        me={{ sm: "auto", md: "20px" }}
-        _focus={{
-          borderColor: { mainTeal },
-        }}
-        _active={{
-          borderColor: { mainTeal },
-        }}
-      >
-        <InputLeftElement
-          children={
-            <IconButton
-              bg="inherit"
-              borderRadius="inherit"
-              _hover="none"
-              _active={{
-                bg: "inherit",
-                transform: "none",
-                borderColor: "transparent",
-              }}
-              _focus={{
-                boxShadow: "none",
-              }}
-              icon={<SearchIcon color={searchIcon} w="15px" h="15px" />}
-            ></IconButton>
-          }
-        />
-        <Input
-          fontSize="xs"
-          py="11px"
-          color={mainText}
-          placeholder="Type here..."
-          borderRadius="inherit"
-        />
-      </InputGroup>
       <NavLink to="/auth/signin">
         <Button
           ms="0px"
           px="0px"
-          me={{ sm: "2px", md: "16px" }}
+          me={{ sm: '2px' }}
           color={navbarIcon}
           variant="transparent-with-icon"
           rightIcon={
             document.documentElement.dir ? (
-              ""
+              ''
             ) : (
               <ProfileIcon color={navbarIcon} w="22px" h="22px" me="0px" />
             )
@@ -111,12 +69,10 @@ export default function HeaderLinks(props) {
             document.documentElement.dir ? (
               <ProfileIcon color={navbarIcon} w="22px" h="22px" me="0px" />
             ) : (
-              ""
+              ''
             )
           }
-        >
-          <Text display={{ sm: "none", md: "flex" }}>Sign In</Text>
-        </Button>
+        ></Button>
       </NavLink>
       <SidebarResponsive
         logoText={props.logoText}
@@ -127,7 +83,7 @@ export default function HeaderLinks(props) {
       />
       <SettingsIcon
         cursor="pointer"
-        ms={{ base: "16px", xl: "0px" }}
+        ms={{ base: '16px', xl: '0px' }}
         me="16px"
         ref={settingsRef}
         onClick={props.onOpen}
@@ -172,12 +128,12 @@ export default function HeaderLinks(props) {
         </MenuList>
       </Menu>
     </Flex>
-  );
+  )
 }
 
 HeaderLinks.propTypes = {
   variant: PropTypes.string,
   fixed: PropTypes.bool,
   secondary: PropTypes.bool,
-  onOpen: PropTypes.func,
-};
+  onOpen: PropTypes.func
+}
